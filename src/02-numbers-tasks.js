3e1b4c3717d1c90f06b1668cd721e9d0bde8b6a9
Gleb <3696732@mail.ru>
2019-12-19 23:40:47 +0300

    feat: add solution for second task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -116,8 +116,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  console.log(x1, y1, x2, y2);
-  return (x1 * x2 + y1 * y2) / (Math.sqrt(x1 ** 2 + x2 ** 2) * Math.sqrt(y1 ** 2 + y2 ** 2));
+  const len1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const len2 = Math.sqrt(x2 * x2 + y2 * y2);
+  const scalarPr = x1 * x2 + y1 * y2;
+  return Math.acos(scalarPr / (len1 * len2));
 }
 
 /**
@@ -132,8 +134,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const str = `${value}`;
+  return parseInt(str[str.length - 1], 10);
 }
 
 
@@ -148,8 +151,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return parseFloat(value);
 }
 
 /**
@@ -165,8 +168,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -186,8 +189,8 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -207,8 +210,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0 && n !== i) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -226,8 +234,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number.isNaN(Math.abs(value)) ? def : Math.abs(value);
 }
 
 module.exports = {
